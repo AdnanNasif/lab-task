bb41adf7061d315a596d62defb4a28ed0da80f4f
AdnanNasif <78008037+AdnanNasif@users.noreply.github.com>
2021-02-07 01:04:53 -0700

    Add files via upload

---
+++ lab/lab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Student
+{
+    public String name;
+    public int id;
+    public String depertment;
+    public float cgpa;
+
+    public void ShowDetails()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("ID " + id);
+        Console.WriteLine("Depertment" + depertment);
+        Console.WriteLine("CGPA " + cgpa);
