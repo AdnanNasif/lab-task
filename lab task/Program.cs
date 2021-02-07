1df39b29b09f4e2697d8aa1ecd7e18d8ebef9015
aisheryo adnan <joseph.nasif@gmail.com>
2021-02-07 12:03:44 +0600

    lab

---
+++ lab task/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ModuleTwoAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int column = 0; column < 4; column++)  
+            {
+                for (int row = 0; row < 6; row++)
+                {
+                    if ((row + column) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else if ((row + column) % 2 == 1)
+                    {
+                        Console.Write("O");
+                    }
+                    else
+                    {
+                        Console.Write("Neither X nor O");  
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
